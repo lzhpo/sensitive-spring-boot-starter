ae77675bdd5a1688c821608b1c1c77e2a29b0d5a
lzhpo <lzhpo1024@gmail.com>
2023-03-26 12:46:49 +0800

    Rename FastJson2BeanPostProcessor bean

--- src/main/java/com/lzhpo/sensitive/FastJson2AutoConfiguration.java
+++ src/main/java/com/lzhpo/sensitive/FastJson2AutoConfiguration.java
@@ -32,7 +32,7 @@ import org.springframework.context.annotation.Configuration;
 public class FastJson2AutoConfiguration {
 
     @Bean
-    public FastJson2BeanPostProcessor httpMessageConverterBeanPostProcessor() {
+    public FastJson2BeanPostProcessor fastJson2BeanPostProcessor() {
         return new FastJson2BeanPostProcessor();
     }
 }
